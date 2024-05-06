6f22fe801bc3c68e3871feb85a534cd97e122dfc
DUAN <134042675+DANNHIROAKI@users.noreply.github.com>
2024-05-07 05:58:58 +1000

    Ungrade

--- repeat.py
+++ repeat.py
@@ -1,4 +1,9 @@
 import subprocess
 
+count = 0  # 初始化计数器
 while True:
-    subprocess.run(["D:/Python_Environment/Anaconda3/envs/EODP-2/python.exe", "f:/Destop/Project2/%main.py"])
+    count += 1  # 每次循环开始时计数器加1
+    # 运行Python脚本
+    subprocess.run(["D:/Python_Environment/Anaconda3/envs/EODP-2/python.exe", "f:/Destop/新建文件夹/%main.py"])
+    # 打印已执行的轮数
+    print(f"已执行 {count} 轮")
